c7e3c1a9d43fe979ed7ea79bcd446d48b3e948dd
Lraty <lraty@outlook.com>
2022-12-15 19:59:52 +0800

    mine page

--- campus_team_up/lib/routes/route.dart
+++ campus_team_up/lib/routes/route.dart
@@ -1,6 +1,7 @@
 import 'package:campus_team_up/ui/discovery_page/discoveryPage.dart';
 import 'package:campus_team_up/ui/inbox_page/inboxPage.dart';
 import 'package:campus_team_up/ui/login_sign_up_page/loginSignUpPage.dart';
+import 'package:campus_team_up/ui/mine_page/minePage.dart';
 import 'package:campus_team_up/ui/promulgate_page/promulgatePage.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:get/get_navigation/src/routes/observers/route_observer.dart';
@@ -11,6 +12,7 @@ class RouteManager {
     GetPage(name: '/discovery', page: () => DiscoveryPage()),
     GetPage(name: '/promulgate', page: () => PromulgatePage()),
     GetPage(name: '/inbox', page: () => InboxPage()),
+    GetPage(name: '/mine', page: () => MinePage()),
     
     GetPage(name: '/home', page: () => LoginSignUpPage()),
     GetPage(name: '/login', page: () => LoginSignUpPage()),
